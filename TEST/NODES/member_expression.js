3a164941288704bc1fa651e876c2f136e4b745d4
dli <ihgazni2010@hotmail.com>
2019-08-27 00:27:07 +0000

    label_statement

--- TEST/NODES/member_expression.js
+++ TEST/NODES/member_expression.js
@@ -20,3 +20,51 @@ Node {
   computed: true }
 >
 
+
+    > d=ac.parse(`exports["unsigned short"] = createIntegerConversion(16, { unsigned: true });`,{sourceType:"module"})
+Node {
+  type: 'Program',
+  start: 0,
+  end: 76,
+  body:
+   [ Node {
+       type: 'ExpressionStatement',
+       start: 0,
+       end: 76,
+       expression: [Object] } ],
+  sourceType: 'module' }
+>
+> d.body[0].expression
+Node {
+  type: 'AssignmentExpression',
+  start: 0,
+  end: 75,
+  operator: '=',
+  left:
+   Node {
+     type: 'MemberExpression',
+     start: 0,
+     end: 25,
+     object: Node { type: 'Identifier', start: 0, end: 7, name: 'exports' },
+     property:
+      Node {
+        type: 'Literal',
+        start: 8,
+        end: 24,
+        value: 'unsigned short',
+        raw: '"unsigned short"' },
+     computed: true },
+  right:
+   Node {
+     type: 'CallExpression',
+     start: 28,
+     end: 75,
+     callee:
+      Node {
+        type: 'Identifier',
+        start: 28,
+        end: 51,
+        name: 'createIntegerConversion' },
+     arguments: [ [Object], [Object] ] } }
+>
+
