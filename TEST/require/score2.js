e5537cc7910806c858bcffc78b9f5064fd6ff8e8
ihgazni2 <286264978@qq.com>
2020-01-14 08:46:32 +0800

    Create score2.js

---
+++ TEST/require/score2.js
@@ -0,0 +1,31 @@
+const level_opts = [
+    {
+        checked:false,
+        bgclr:"green",
+        ch:"优秀"
+    },
+    {
+        checked:false,
+        bgclr:"#1296db",
+        ch:"较好"
+    },
+    {
+        checked:false,
+        bgclr:"yellow",
+        ch:"合格"
+    },
+    {
+        checked:false,
+        bgclr:"orange",
+        ch:"较差"
+    },
+    {
+        checked:false,
+        bgclr:"red",
+        ch:"不合格"
+    },
+]
+
+module.exports =  {
+    level_opts,
+}
