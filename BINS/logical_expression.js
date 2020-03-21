7859765f4e4b0410bbb96f53807eded67ec139f5
dli <terryinzaghi@163.com>
2020-03-21 07:11:56 +0000

    logical_expression

--- BINS/logical_expression.js
+++ BINS/logical_expression.js
@@ -2,7 +2,7 @@
 const engine1 = require("./bin_engine_1.js")
 
 let attribs = ["operator","left","right"]
-let type = "BinaryExpression"
+let type = "LogicalExpression"
 let funcs = [engine1.doNothing,engine1.maybeNodeToCode,engine1.maybeNodeToCode]
 
 engine1.go({
